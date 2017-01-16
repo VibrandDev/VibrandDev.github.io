a2d3997ad5434b4e2d8504e66d08086cd8895e9c
BaobrandSA <james@baobrand.co.za>
2017-01-16 11:59:31 +0200

    update

--- js/owl.custom.js
+++ js/owl.custom.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
  
   $("#owl-demo").owlCarousel({
  
-      autoPlay: 3000, //Set AutoPlay to 3 seconds
+      autoPlay: 1000, //Set AutoPlay to 3 seconds
  
-      items : 4,
+      items : 5,
       itemsDesktop : [1199,3],
       itemsDesktopSmall : [979,3],
       pagination : false,
